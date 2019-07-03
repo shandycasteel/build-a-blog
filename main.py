1cf695ebbebf7ef9a26d8033f5ab0933601315b7
Shandy Casteel <shandycasteel@gmail.com>
2019-07-02 23:52:50 -0500

    Add date to Blog class, drop/recreate table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -12,39 +13,36 @@ db = SQLAlchemy(app)
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    title =  db.Column(db.Text, nullable=False)
-    body = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String(120))
+    body = db.Column(db.Text)
+    pub_date = db.Column(db.DateTime, nullable=False,
+        default=datetime.utcnow)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, pub_date):
         self.title = title
         self.body = body
+        self.pub_date = pub_date
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/blog")
 def index():
 
-    if request.method == "POST":
-        task_name = request.form["task"]
-        new_task = Task(task_name)
-        db.session.add(new_task)
-        db.session.commit()
+    posts_list = Blog.query.get(all)
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template("todos.html", title="Get It Done",
-        tasks=tasks, completed_tasks=completed_tasks)
+    return render_template('blog.html', posts=posts_list)
 
+@app.route('/newpost', methods=['POST', 'GET'])
+def create_post():
 
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
+    # Create empty strings for error messages
+    title_error = ""
+    body_error = ""
 
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect('/')
+    # Assign variables to arguments from newpost form
+    post_title = request.form['blog_title']
+    post_body =  request.form['blog_body']
+    post_date = request.form['blog_date']
+    new_post =  Blog(post_title, post_body, post_date)
 
 
 if __name__ == "__main__":
