281d5857ddb5f6f5d5785132322755a8e877ade1
Shandy Casteel <shandycasteel@gmail.com>
2019-07-10 23:42:01 -0500

    Simplify, remove postdate, add base template, test form and display functions for posts

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -15,35 +14,23 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
-    pub_date = db.Column(db.DateTime, nullable=False,
-        default=datetime.utcnow)
 
-    def __init__(self, title, body, pub_date):
+    def __init__(self, title, body):
         self.title = title
         self.body = body
-        self.pub_date = pub_date
 
 
-@app.route("/blog")
+@app.route('/', methods=['POST', 'GET'])
 def index():
 
-    posts_list = Blog.query.get(all)
+    if request.method == 'POST':
+        new_post = Blog(request.form['title'], request.form['body'])
+        db.session.add(new_post)
+        db.session.commit()
 
-    return render_template('blog.html', posts=posts_list)
+    all_posts = Blog.query.all()
 
-@app.route('/newpost', methods=['POST', 'GET'])
-def create_post():
-
-    # Create empty strings for error messages
-    title_error = ""
-    body_error = ""
-
-    # Assign variables to arguments from newpost form
-    post_title = request.form['blog_title']
-    post_body =  request.form['blog_body']
-    post_date = request.form['blog_date']
-    new_post =  Blog(post_title, post_body, post_date)
-
+    return render_template("base.html", all_posts=all_posts)
 
 if __name__ == "__main__":
     app.run()
