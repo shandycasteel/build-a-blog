0dc4d9f863fb5ac74ef29075536fae3914dec9a7
Shandy Casteel <shandycasteel@gmail.com>
2019-07-11 12:16:45 -0500

    Add single_post functionaility, finish styling, test

--- main.py
+++ main.py
@@ -23,10 +23,14 @@ class Blog(db.Model):
 
 @app.route("/blog")
 def index():
+    post_id = request.args.get('id')
 
-    all_posts = Blog.query.all()
-
-    return render_template("blog.html", all_posts=all_posts)
+    if post_id:
+        single_post = Blog.query.get(post_id)
+        return render_template('single_post.html', single_post=single_post)
+    else:
+        all_posts = Blog.query.all()
+        return render_template("blog.html", all_posts=all_posts)
 
 @app.route("/newpost", methods=["POST", "GET"])
 def add_post():
@@ -34,14 +38,17 @@ def add_post():
     if request.method == "POST":
         new_title = request.form["title"]
         new_body = request.form["body"]
+        new_post = Blog(new_title, new_body)
 
         if len(new_title) != 0 and len(new_body) != 0:
-            new_post = Blog(new_title, new_body)
             db.session.add(new_post)
             db.session.commit()
-            return redirect("/blog")
+
+            post_link = f"/blog?id={new_post.id}"
+            return redirect(post_link)
+
         else:
-            flash("Posts require both a title and a body . . . try again!")
+            flash("Posts require both a title and a body...try again!")
             return render_template("add_post.html", new_title=new_title, new_body=new_body, title="Add a Blog Entry")
 
     else:
